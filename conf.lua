5e00b0f0318396724cb5f52cc3743b8485a8f12b
= <=>
2014-12-25 10:58:03 -0500

    Removing docs for FOV scaling inputs

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.identity = "vividcast_demo"      -- The name of the save directory (string)
-  t.window.title = "VividCast Demo - move1:wasdqe, move2:ijkluo, FOV:[], Resolution:-="  -- The window title (string)
+  t.window.title = "VividCast Demo - move1:wasdqe, move2:ijkluo, Resolution:-="  -- The window title (string)
   t.window.width = (512+64)*2             -- The window width (number)
   t.window.height = 512+64          -- The window height (number)
   t.window.resizable = true         -- Let the window be user-resizable (boolean)
